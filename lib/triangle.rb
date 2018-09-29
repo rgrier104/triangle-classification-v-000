b5402be31a0818bcb560eeb35703cb1511a636a7
Rachel Grier <rgrier104@gmail.com>
2018-09-29 22:39:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,8 @@ class Triangle
   end
 
   def kind
-    if @side1 <= 0 or @side2 <= 0 or @side3 <= 0
+    if (@side1 <= 0 or @side2 <= 0 or @side3 <= 0) or
+      (@side1 + @side2 < @side3) or (@side1 + @side3 < @side2) or (@side2 + @side3 < @side1)
         raise TriangleError
     elsif
       @side1 == @side2 and @side3 == @side2
